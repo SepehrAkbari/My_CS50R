f4c786e49f950f973d3ed913b167bff088aa5622
Sepehr Akbari <82570094+SepehrAkbari@users.noreply.github.com>
2024-07-16 18:56:16 +0330

    Create coffeehouse.R

---
+++ ProblemSet_1/Teehouse/coffeehouse.R
@@ -0,0 +1,9 @@
+x <- readline("Do you like coffee? ")
+
+if (x == "Yes") {
+  print("black coffee")
+} else if (x == "No") {
+  print("Get out")
+} else {
+  print("Please enter either 'Yes' or 'No'")
+}
